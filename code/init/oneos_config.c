7fa5fb133e0309df9bbda34000e8a80ff9940b9e
wgj1922 <wgj1922@hotmail.com>
2017-03-25 22:22:13 +0800

    commit oneos code

---
+++ code/init/oneos_config.c
@@ -0,0 +1,13 @@
+/*
+ *  init/oneos_config.c
+ *
+ *  Copyright (C) 2015, Will Wan
+ *
+ *  Descroption: config ONEOS
+ *  2015/10/28: Create this file. 
+ */
+
+int one_config_os(void)
+{
+	
+}
---
+++ code/init/oneos_init.c
@@ -0,0 +1,13 @@
+/*
+ *  init/oneos_init.c
+ *
+ *  Copyright (C) 2015, Will Wan
+ *
+ *  Descroption: init ONEOS
+ *  2015/10/28: Create this file. 
+ */
+
+int one_init_os(void)
+{
+	
+}
